ff399b4e71c98ec939522bc538c5d84193ec848d
premchand-ftr <ppp439@ftr.com>
2023-03-14 06:38:58 +0530

    initial

---
+++ churn-model.py
@@ -0,0 +1,47 @@
+import pandas as pd
+
+from sklearn.ensemble import RandomForestClassifier
+
+import pickle
+
+pd.set_option('display.max_columns', None)
+
+pd.set_option('display.max_rows', None)
+
+df_churn = pd.read_csv('TelcoChurn.csv')
+
+df_churn = df_churn[['gender', 'PaymentMethod', 'MonthlyCharges',
+
+'tenure', 'Churn']].copy()
+
+df = df_churn.copy()
+
+df.fillna(0, inplace=True)
+
+
+encode = ['gender','PaymentMethod']
+
+for col in encode:
+
+    dummy = pd.get_dummies(df[col], prefix=col)
+
+    df = pd.concat([df,dummy], axis=1)
+
+    del df[col]
+import numpy as np
+
+df['Churn'] = np.where(df['Churn']=='Yes', 1, 0)
+
+
+
+X = df.drop('Churn', axis=1)
+
+Y = df['Churn']
+
+
+print(df_churn.head())
+
+clf = RandomForestClassifier()
+
+clf.fit(X, Y)
+pickle.dump(clf, open('churn_clf.pkl', 'wb'))
